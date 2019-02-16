6c2b6cb1473b148d33b8c69539a2ef568b604daa
slmlt <40519588+slmlt@users.noreply.github.com>
2019-02-16 18:15:42 +0200

    Report Generator Implementation.
    Generates Report in one of following forms:
    1) Kartasvalok.ru API report
    2) Kgdesoo.ru API report

---
+++ app/src/main/java/ru/konighack2019/cleancity/service/generator/TestReportGenerator.kt
@@ -0,0 +1,82 @@
+package ru.konighack2019.cleancity.service.generator
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.net.Uri
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.FusedLocationProviderClient
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.kodein.di.generic.instance
+import ru.konighack2019.cleancity.AppDelegate
+import ru.konighack2019.cleancity.R
+import ru.konighack2019.cleancity.model.KSReport
+import ru.konighack2019.cleancity.model.UserInfo
+import ru.konighack2019.cleancity.service.ReportGenerator
+import ru.konighack2019.cleancity.service.common.OperationState
+import java.lang.Exception
+import java.util.*
+
+class TestReportGenerator : ReportGenerator {
+    override fun createEsooReport(imageUris: List<Uri>): LiveData<OperationState> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createKSReport(imageUris: List<Uri>): LiveData<OperationState> {
+        generationState.postValue(OperationState.VALIDATING)
+        generateReport(imageUris)
+        return generationState
+    }
+
+    private val geocoder = Geocoder(AppDelegate.applicationContext(), Locale.getDefault())
+    private val locationProvider: FusedLocationProviderClient by AppDelegate.getKodein().instance()
+
+    private lateinit var pendingReport: KSReport
+    private val generationState = MutableLiveData<OperationState>()
+
+    override fun getState() = generationState
+    override fun getReport() = pendingReport
+
+    private fun generateReport(photo: List<Uri>) {
+
+        GlobalScope.launch {
+            try {
+                if (ContextCompat.checkSelfPermission(
+                        AppDelegate.applicationContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    locationProvider.lastLocation.addOnSuccessListener {
+                        val addr = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+                        //val user = dataService.getUserInfoBlocking()
+                        val user = UserInfo("test", "test@test.io", "+71234567890")
+
+                        pendingReport = KSReport(
+                            AppDelegate.applicationContext().getString(R.string.report_subject),
+                            String.format(AppDelegate.applicationContext().getString(R.string.report_description), addr[0].getAddressLine(0)),
+                            addr[0].getAddressLine(0),
+                            user.name,
+                            user.email,
+                            user.phone,
+                            addr[0].adminArea,
+                            "1",
+                            "0",
+                            it.latitude.toString() + "," + it.longitude.toString(),
+                            photo
+                        )
+                        generationState.postValue(OperationState.SUCCESS)
+                    }
+                }
+            } catch (ex: Exception) {
+                generationState.postValue(OperationState.FAILED)
+            }
+        }
+    }
+
+
+
+}
